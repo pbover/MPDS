f84e36d73c0028cc303d44b00bb1405800abc000
Poncio Bover Castello <pobover@gmail.com>
2023-03-07 03:12:47 +0100

    readme

--- practicas/UNIDAD_40/ServicesContractMenu/src/es/pbover/master/ServicesContractMenu.java
+++ practicas/UNIDAD_40/ServicesContractMenu/src/es/pbover/master/ServicesContractMenu.java
@@ -4,15 +4,6 @@ class ServicesContractMenu extends DynamicMenu {
 
     private ServicesContract servicesContract;
 
-    /*
-     * menu principal
-     * 1.- Imprimir Contratos de Servicios
-     * 2 - Cancelar Contrato de Servicios
-     * 3.- Ampliar Contrato de Servicios
-     * 4.- Desplazar Contrato de Servicios
-     * 5.- Salir
-     */
-
     public ServicesContractMenu(ServicesContract servicesContract) {
         super("Gestión de Contratos de Servicios");
         this.servicesContract = servicesContract;
