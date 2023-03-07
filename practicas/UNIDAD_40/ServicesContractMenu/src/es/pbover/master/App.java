792a180a244cf6a0fdc63191fdaef005256e4b42
Poncio Bover Castello <pobover@gmail.com>
2023-03-07 17:37:54 +0100

    refact pack

--- practicas/UNIDAD_40/ServicesContractMenu/src/es/pbover/master/App.java
+++ practicas/UNIDAD_40/ServicesContractMenu/src/es/pbover/master/App.java
@@ -1,5 +1,9 @@
 package es.pbover.master;
 
+import es.pbover.master.utils.io.Console;
+import es.pbover.master.model.menu.*;
+import es.pbover.master.model.*;
+
 public class App {
   public static void main(String[] args) {
     new ServicesContractMenu(new ServicesContract(new Console().readInt("Introduzca el anyo(yyyy) :"))).interact();
